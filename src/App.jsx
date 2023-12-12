f3f1bdb0364c4aabaa3ca0be4ffbdedb994692dd
ahmedrehane <rehaneahmed49@gmail.com>
2023-12-12 16:00:49 +0100

    click me

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+import Meme from './components/Meme.jsx'
+import data from './components/data.jsx'
+
+
+
+
+function App (){
+  return(
+    <div>
+      
+      
+    <Meme/>
+    
+      
+
+    </div>
+
+  )
+}
+export default App
+
+  
